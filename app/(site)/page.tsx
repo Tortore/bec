9266589fec6944f0f8c4b14edd7615567aab9715
GIOVANINO JOJO <giovaninojojo@GIOVANINOs-MacBook-Air.local>
2026-08-21 13:23:08 +0200

    Retirer l'équipe de la page d'accueil

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,7 +3,6 @@ import { Hero } from "@/components/sections/hero";
 import { RecentProjects } from "@/components/sections/recent-projects";
 import { Services } from "@/components/sections/services";
 import { Stats } from "@/components/sections/stats";
-import { TeamPreview } from "@/components/sections/team-preview";
 import { getCategoryLabels, getHome } from "@/lib/cms/queries";
 
 export default async function HomePage() {
@@ -14,7 +13,6 @@ export default async function HomePage() {
       <Stats stats={home.stats} />
       <Services home={home} />
       <RecentProjects home={home} labels={labels} />
-      <TeamPreview home={home} />
       <CtaBand home={home} />
     </>
   );
